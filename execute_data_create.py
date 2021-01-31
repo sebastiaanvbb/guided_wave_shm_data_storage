f46c93a581663f55e0664c92fc768e2091e405c5
sebastiaanvbb <60063382+sebastiaanvbb@users.noreply.github.com>
2021-01-31 20:10:49 +0100

    Update execute_data_create.py

--- execute_data_create.py
+++ execute_data_create.py
@@ -3,41 +3,26 @@ from coupons_and_fatigue_files import ExtractDataFileNames
 from create_data import VariousCouponsTogether
 
 """
-self note: change saving_location to dataX\BC_n and coupons = ['L1_S11_F' , ...]
+note: change saving_location to dataX\BC_n and coupons = ['L1_S11_F' , ...]
 """
 colab = 0
 conidition = 'loaded'
-if colab == 0:
-    location_of_NASA_composite_files = r'C:\Users\Sebastiaan\PycharmProjects\ML_for_SHM\data'
-    if conidition == 'clamped':
-        saving_location_dframes = r'C:\Users\Sebastiaan\PycharmProjects\ML_for_SHM\data\data_all_sets_together\dataX' \
-                                  r'\data_frames_clamped'
-        saving_location = r'C:\Users\Sebastiaan\PycharmProjects\ML_for_SHM\data\data_all_sets_together\dataX\BC_clamped'
-        # type all name variants in the string below as the excels sheets which
-        # are scanned for data extraction are not typed up consistently
-        bc = ['Clamped', 'clamped', 'Clamped ', 'clamped ', 'Clamped ']
-    elif conidition == 'traction free':
-        saving_location_dframes = r'C:\Users\Sebastiaan\PycharmProjects\ML_for_SHM\data\data_all_sets_together\dataX' \
-                                  r'\data_frames_free'
-        saving_location = r'C:\Users\Sebastiaan\PycharmProjects\ML_for_SHM\data\data_all_sets_together\dataX\\BC_traction_free'
-        bc = ['Traction Free', 'traction free', 'Traction free', 'traction Free', 'Traction Free ']
-    elif conidition == 'loaded':
-        saving_location_dframes = r'C:\Users\Sebastiaan\PycharmProjects\ML_for_SHM\data\data_all_sets_together\dataX' \
-                                  r'\data_frames_loaded'
-        saving_location = r'C:\Users\Sebastiaan\PycharmProjects\ML_for_SHM\data\data_all_sets_together\dataX\BC_loaded'
-        bc = ['Loaded', 'loaded', 'Loaded ', 'loaded ', ' loaded']
-else:
-    location_of_NASA_composite_files = r'/content/gdrive/My Drive/composites'
-    saving_location_dframes = r'/content/gdrive/My Drive/data_sets_comps/data_frames/data_frames'
-    saving_location = r'/content/gdrive/My Drive/data_sets_comps/dataX'
+
+location_of_NASA_composite_files = r'C:\...\data'
+
+if conidition == 'loaded':
+    saving_location_dframes = r'C:\...'
+    saving_location = r'C:\...'
+    bc = ['Loaded', 'loaded', 'Loaded ', 'loaded ', ' loaded'] 
+    # add all variants of boundary condition name due to dissimilarities in Excel data
+
 
 if __name__ == '__main__':
 
     # =======================================SelectData======================================================== #
 
-    # coupons = ['L2_S11_F', 'L2_S17_F', 'L2_S18_F', 'L2_S20_F']
     coupons = ['L1_S11_F', 'L1_S12_F', 'L1_S18_F', 'L1_S19_F']
-    # coupons = ['L3_S11_F', 'L3_S13_F', 'L3_S18_F', 'L3_S20_F']  # L3S14_0_0 is incomplete and not used
+    
     # =====================================ExtractDataFileNames================================================ #
 
     create_names = ExtractDataFileNames(saving_location_dframes, location_of_NASA_composite_files, coupons, bc)
